0717b314eab7f7e6f8c71fd51662879a1872a610
Greg Westneat <gregory.westneat@sweetgreen.com>
2022-11-04 19:39:04 -0700

    fix: tests:

--- apps/test/jest.config.ts
+++ apps/test/jest.config.ts
@@ -1,7 +1,7 @@
 module.exports = {
   displayName: 'test',
   resolver: '@nrwl/jest/plugins/resolver',
-  preset: 'react-native',
+
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
@@ -10,4 +10,19 @@ module.exports = {
   moduleNameMapper: {
     '.svg': '@nrwl/expo/plugins/jest/svg-mock',
   },
+
+  projects: [
+    { preset: 'jest-expo/ios' },
+    { preset: 'jest-expo/android' },
+    {
+      preset: 'jest-expo/web',
+      // will only run for files with .web
+      testMatch: [
+        `**/__tests__/**/*spec.web.[jt]s?(x)`,
+        `**/__tests__/**/*test.web.[jt]s?(x)`,
+        `**/?(*.)+(spec|test).web.[jt]s?(x)`,
+      ],
+    },
+  ],
+  preset: 'react-native',
 };
